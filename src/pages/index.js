7e2ef0e1103251e1ffa867a2501c0edf37d4544c
jamieroszel <jamie.s.roszel@gmail.com>
2021-07-10 21:39:44 -0400

    style importing now works, have to use *

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,17 @@
 import React from "react"
 import { Link } from 'gatsby'
 import Layout from "../components/Layout"
-import styles from '../styles/home.module.css'
-
+import * as styles from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>Technical Writer based in Apex, NC</p>
-          <Link to="/topics">Now...go learn all the things</Link>
+          <Link className={styles.btn} to="/topics">Now...go learn all the things</Link>
         </div>
       </section>
     </Layout>
--- src/pages/topics/index.js
+++ src/pages/topics/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import Layout from "../../components/Layout"
+import * as styles from '../../styles/projects.module.css'
 
 
 const Topics = () => {
     return (
         <Layout>
-            <div>
+            <div className={styles.portfolio}>
                 <h2>Topics</h2>
                 <h3>Various Topics</h3>
             </div>
         </Layout>
-    )
+    );
 }
 
 export default Topics
