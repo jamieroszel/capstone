d8fa1b6ead3c4e1f2cd1a322f620508d3b99b581
jamieroszel <jamie.s.roszel@gmail.com>
2021-07-11 22:29:16 -0400

    everything broken. 9 errors

--- src/templates/article-details.js
+++ src/templates/article-details.js
@@ -2,15 +2,16 @@ import React from 'react'
 import Layout from '../components/Layout'
 import styles from "../styles/article-details.module.css"
 
-export default function ArticleDetails() {
+const ArticleDetails=() => {
     return (
         <Layout>
             <div className={styles.details}>
                 <h2>title</h2>
-                <h2>description</h2>
-                <h2>title</h2>
+                <h3>description</h3>
             </div>
-            <div className={styles.html} dangerouslySetInnerHTML={} />
+            {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
         </Layout>
     )
 }
+
+export default ArticleDetails
